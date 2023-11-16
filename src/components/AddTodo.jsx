bc43f12838b8db28cf0a92375a308dd74da4a404
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-16 06:33:51 +0530

    add "onclick" in button and taking input by clicking it

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -3,17 +3,29 @@ export function AddTodo({ onNewItem }) {
   const [todoName,setTodoName]=useState()
   const [dueDate,setDueDate] =useState()
 
+  const handleNameChange = (event)=> {
+    setTodoName(event.target.value);
+  }
+  const handleDateChange = (event)=> {
+    setDueDate(event.target.value);
+  }
+  const addButtonOnClicked = ()=>{
+    onNewItem(todoName,dueDate);
+    setTodoName("")
+    setDueDate("");
+  }
+
   return (
     <div className="container text-center">
       <div className="row My-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here" />
+          <input type="text" placeholder="Enter Todo Here"  onChange={handleNameChange}/>
         </div>
         <div className="col-4">
-          <input type="date" />
+          <input type="date"  onChange={handleDateChange}/>
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success My-btn" onClick={onNewItem()}>
+          <button type="button" className="btn btn-success My-btn" onClick={addButtonOnClicked}>
             Add
           </button>
         </div>
